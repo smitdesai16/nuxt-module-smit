af84196a9a88c5cd568a086d2884a97343dc0d6f
Smit Desai <smit.desai16@gmail.com>
2020-05-20 15:48:04 -0500

    Adding nuxt-property-decorator

--- components/global.vue
+++ components/global.vue
@@ -1,3 +1,18 @@
 <template>
 	<h1>This is global tag</h1>
 </template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Footer extends Vue {
+	@Prop({ default: '' }) projectStartYear: string | undefined
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+	color: red;
+}
+</style>
